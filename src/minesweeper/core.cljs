83fab897ab87c5e690e91e9c1f7dc3caac7ec411
Rob Jinman <admin@robjinman.com>
2016-09-09 21:52:25 +0100

    Sweep single cells, multiple 'spread' sweeps, flag mines, clear zero regions

--- src/minesweeper/core.cljs
+++ src/minesweeper/core.cljs
@@ -1,35 +1,39 @@
 (ns minesweeper.core
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [om.dom :as dom]
+            [clojure.set]))
 
 (enable-console-print!)
 
-(def rows 20)
-(def cols 30)
-(def num-mines 40)
+(def rows 10)
+(def cols 10)
+(def num-mines 10)
 
 (def empty-row (vec (repeat cols 0)))
 
 ;; A number between 0 and 8 corresponds to the number of surrounding mines.
 ;; A value of :X represents a mine.
-(def initial-board (vec (repeat rows empty-row)))
+(def empty-grid (vec (repeat rows empty-row)))
 
-;; A 0 represents a hidden square, a 1 represents a revealed square
-(def initial-mask (vec (repeat rows empty-row)))
+;; 1 = revealed cell, 0 = hidden cell
+(def empty-mask (vec (repeat rows empty-row)))
 
-(defn populate-board
-  "Takes an empty board and populates it randomly with mines"
-  [board num-mines]
+;; 1 = flag, 0 = no flag
+(def empty-flags (vec (repeat rows empty-row)))
+
+(defn populate-grid
+  "Takes an empty grid and populates it randomly with mines"
+  [grid num-mines]
   (loop [i 0
-         board board]
+         grid grid]
     (let [row (rand rows)
           col (rand cols)
-          is-mine (= :X (get-in board [row col]))]
+          is-mine (= :X (get-in grid [row col]))]
       (if (< i num-mines)
         (recur (if is-mine i (inc i))
-               (if is-mine board (assoc-in board [row col] :X)))
-        board))))
+               (if is-mine grid (assoc-in grid [row col] :X)))
+        grid))))
 
 (defn neighbour-coords
   [row col]
@@ -43,17 +47,25 @@
             []
             indices)))
 
-(defn flatten-board
+(defn count-nearby-flags
+  [flags row col]
+  (reduce (fn [count cell]
+            (let [flag (get-in flags cell)]
+              (+ count flag)))
+          0
+          (neighbour-coords row col)))
+
+(defn flatten-grid
   "Returns a flat vector of [row col value] vectors to enable easy
-  iteration over the board"
-  [board]
-  (for [[i row] (map-indexed vector board)
+  iteration over the grid"
+  [grid]
+  (for [[i row] (map-indexed vector grid)
         [j val] (map-indexed vector row)]
     [i j val]))
 
-(defn label-board
-  "Takes a populated board and sets the numbers"
-  [board]
+(defn label-grid
+  "Takes a populated grid and sets the numbers"
+  [grid]
   (reduce (fn [labelled [i j val]]
             (if (= :X val)
               (let [neighbours (neighbour-coords i j)]
@@ -63,118 +75,223 @@
                         labelled
                         neighbours))
               labelled))
-          board
-          (flatten-board board)))
+          grid
+          (flatten-grid grid)))
+
+(defn player-victory?
+  [grid mask]
+  (= num-mines 
+     (reduce (fn [total [r c n]]
+               (if (= 0 n)
+                 (inc total)
+                 total))
+             0
+             (flatten-grid mask))))
+
+(defn new-state []
+  {:rows rows
+   :cols cols
+   :grid (label-grid (populate-grid empty-grid num-mines))
+   :mask empty-mask
+   :flags empty-flags
+   ;; Valid values are: :alive :dead :victorious
+   :game-state :alive})
 
 (def app-state
-  (atom
-   {:rows rows
-    :cols cols
-    :board (label-board (populate-board initial-board num-mines))
-    :mask (assoc-in initial-mask [0 0] 1)
-    :foo {:bar "hello"}
-    :animals/list
-    [[1 "Ant"] [2 "Antelope"] [3 "Bird"] [4 "Cat"] [5 "Dog"]
-     [6 "Lion"] [7 "Mouse"] [8 "Monkey"] [9 "Snake"] [10 "Zebra"]]}))
+  (atom (new-state)))
 
-(defui Square-vw
+(defui ExposedCellView
   static om/IQuery
   (query [this]
-         [:board :mask])
+         [])
   Object
   (render [this]
-          (let [{:keys [board mask row col]} (om/props this)
-                board-val (get-in board [row col])
-                mask-val (get-in mask [row col])]
-            (println (str "Square rendered! row=" row ",col=" col))
-            (dom/span
-             #js {:className (str "square"
-                                  (if-not (= 1 mask-val) " hidden"))
-                  :onClick (fn [e] (om/transact! this `[(sweep {:row ~row :col ~col})]))}
-             (if (= :X board-val)
-               "X"
-               board-val)))))
+          (let [{:keys [row col value]} (om/get-computed this)]
+            (println (str "Rendering exposed cell (" row "," col "," value ")"))            
+            (dom/div #js {:className "cell revealed"
+                          :onClick (fn [e] (om/transact! this `[(clear {:row ~row :col ~col}) :mask :game-state]))}
+                     (dom/span nil
+                               (if (= :X value)
+                                 "X"
+                                 value))))))
 
-(def square-vw (om/factory Square-vw))
+(def exposed-cell-view (om/factory ExposedCellView))
 
-(defui Thing
+(defui HiddenCellView
   static om/IQuery
   (query [this]
-         [:bar])
+         [])
   Object
   (render [this]
-          (let [{:keys [bar]} (om/props this)]
-               (dom/span nil bar))))
+          (let [{:keys [row col flag]} (om/get-computed this)]
+            (println (str "Rendering hidden cell (" row "," col "," flag ")"))
+            (if (= 1 flag)
+              (dom/div
+               #js {:className "cell hidden flagged"
+                    :onContextMenu (fn [e] (do (.preventDefault e)
+                                               (om/transact! this `[(unflag {:row ~row :col ~col}) :flags])))})
+              (dom/div
+               #js {:className "cell hidden"
+                    :onClick (fn [e] (om/transact! this `[(sweep {:row ~row :col ~col}) :mask :game-state]))
+                    :onContextMenu (fn [e] (do (.preventDefault e)
+                                               (om/transact! this `[(flag {:row ~row :col ~col}) :flags])))})))))
 
-(def thing (om/factory Thing))
+(def hidden-cell-view (om/factory HiddenCellView))
 
-(defui MainView
+(defui CellView
   static om/IQuery
   (query [this]
-         [{:foo (om/get-query Thing)}])
+         [])
   Object
   (render [this]
-          (let [t (:foo (om/props this))]
-            (println (str "foo=" t))
-            (thing t))))
+          (let [{:keys [row col flag value mask-val]} (om/get-computed this)]
+            (if (= mask-val 1)
+              (exposed-cell-view (om/computed {} {:value value :row row :col col}))
+              (hidden-cell-view (om/computed {} {:flag flag :row row :col col}))))))
 
-(defui Board-vw
+(def cell-view (om/factory CellView))
+
+(defui GridView
   static om/IQuery
   (query [this]
-         [:rows :cols :board :mask])
+         [:rows :cols :grid :mask :flags])
   Object
   (render [this]
-          (let [{:keys [rows cols board mask] :as props} (om/props this)
+          (let [{:keys [rows cols grid mask flags] :as props} (om/props this)
                 key #(+ (* %1 7) %2)] ;; Generate unique key from row and col
-            (println "Board rendered!")
+            (println "Rendering grid")
             (apply dom/div
                    #js {:className "minesweeper"}
                    (map-indexed (fn [r row] (apply dom/div
                                    #js {:className "row"}
                                    (map-indexed (fn [c val]
-                                                  (square-vw {:row r
-                                                              :col c
-                                                              :board board
-                                                              :mask mask
-                                                              :react-key (key r c)}))
+                                                  (cell-view (om/computed {:react-key (key r c)}
+                                                                          {:flag (get-in flags [r c])
+                                                                           :value (get-in grid [r c])
+                                                                           :mask-val (get-in mask [r c])
+                                                                           :row r
+                                                                           :col c})))
                                                 row)))
-                                board)))))
+                                grid)))))
+
+(def grid-view (om/factory GridView))
+
+(defui MainView
+  static om/IQuery
+  (query [this]
+         [:rows :cols :grid :mask :flags :game-state])
+  Object
+  (render [this]
+          (let [{:keys [rows cols grid mask flags game-state]} (om/props this)]
+            (println "Rendering main view")
+            (dom/div #js {:className (str "minesweeper-wrap"
+                                          (case game-state
+                                            :victorious " st-victorious"
+                                            ""))}
+                     (dom/button
+                      #js {:onClick (fn [e] (om/transact! this `[(reset)]))}
+                      "Reset")
+                     (grid-view {:rows rows
+                                 :cols cols
+                                 :grid grid
+                                 :mask mask
+                                 :flags flags})))))
+
+(defn player-death!
+  [state [row col]]
+  (swap! state assoc :game-state :dead))
+
+;; TODO: Instead of modifying the state here, have this function
+;; return the set of cells
+(defn sweep-zeros-r!
+  "Sweep this zero's neighbours and any zeros around it"
+  [state [row col] swept]
+  (if (contains? swept [row col])
+    swept
+    (reduce (fn [swept [nx ny]]
+              (do (swap! state assoc-in [:mask nx ny] 1)
+                  (if (= 0 (get-in @state [:grid nx ny]))
+                    (clojure.set/union swept (sweep-zeros-r! state [nx ny] swept))
+                    swept)))
+            (conj swept [row col])
+            (neighbour-coords row col))))
+
+(defn sweep-zeros!
+  [state [row col]]
+  (sweep-zeros-r! state [row col] #{}))
+
+(defn sweep-cell!
+  [state [row col]]
+  (swap! state assoc-in [:mask row col] 1)
+
+  (if (= 0 (get-in @state [:grid row col]))
+    (sweep-zeros! state [row col])
+
+    (if (= :X (get-in @state [:grid row col]))
+      (player-death! state [row col]))))
+
+(defn sweep-spread!
+  [state [row col]]
+  (let [value (get-in @state [:grid row col])
+        num-flags (count-nearby-flags (get @state :flags) row col)]
+    (println (str "value=" value ",num-flags=" num-flags))
+    (if (= value num-flags)
+      (doseq [cell (neighbour-coords row col)]
+        (if (not= 1 (get-in @state (concat [:flags] cell)))
+          (sweep-cell! state cell))))))
+
+(defn flag-cell!
+  [state [row col]]
+  (println (str "Flagging cell " row "," col))
+  (swap! state assoc-in [:flags row col] 1))
+
+(defn unflag-cell!
+  [state [row col]]
+  (println (str "Unflagging cell " row "," col))
+  (swap! state assoc-in [:flags row col] 0))
+
+(defn restart-game!
+  [state]
+  (reset! state (new-state)))
+
+(defn update-game-state!
+  [{:keys [grid mask] :as state}]
+  (println "Updating game state")
+  (if (player-victory? grid mask)
+    (swap! state assoc-in [:game-state] :victorious)))
 
 (defmulti read (fn [env key params] key))
-
-(defn read-query
-  [state query])
-
-(defn read-key
-  [state key])
 
 (defmethod read :default
   [{:keys [state query] :as env} key params]
   (let [st @state]
+    (println (str "key=" key))
     (if-let [[_ v] (find st key)]
       {:value v}
       {:value :not-found})))
-
-(defmethod read :animals/list
-  [{:keys [state] :as env} key {:keys [start end]}]
-  (println (str "start=" start ",end=" end))
-  {:value (subvec (:animals/list @state) start end)})
-
-(defmethod read :boardval
-  [{:keys [state]} key {:keys [row col]}]
-  {:value (get-in (:board @state) [row col])})
-
-(defmethod read :maskval
-  [{:keys [state]} key {:keys [row col]}]
-  {:value (get-in (:mask @state) [row col])})
 
 (defmulti mutate (fn [env key params] key))
 
 (defmethod mutate `sweep
   [{:keys [state]} key {:keys [row col]}]
-  (let [{:keys [mask]} @state]
-    (println "row=" row ",col=" col)
-    {:action #(swap! state assoc-in [:mask row col] 1)}))
+  {:action #(do (sweep-cell! state [row col])
+                (update-game-state! state))})
+
+(defmethod mutate `reset
+  [{:keys [state]} key params]
+  {:action #(restart-game! state)})
+
+(defmethod mutate `clear
+  [{:keys [state]} key {:keys [row col]}]
+  {:action #(sweep-spread! state [row col])})
+
+(defmethod mutate `flag
+  [{:keys [state]} key {:keys [row col]}]
+  {:action #(flag-cell! state [row col])})
+
+(defmethod mutate `unflag
+  [{:keys [state]} key {:keys [row col]}]
+  {:action #(unflag-cell! state [row col])})
 
 (def parser (om/parser {:read read :mutate mutate}))
 
@@ -184,4 +301,4 @@
     :parser parser}))
 
 (om/add-root! reconciler
-              Board-vw (gdom/getElement "app"))
+              MainView (gdom/getElement "app"))
